57d9303760ae94c64757229ee79d0cb968dc26e8
Oktay TONTAŞ <oktay1125@gmail.com>
2020-07-16 16:58:39 +0300

    hh

---
+++ risaleOnline/wwwroot/js/language.js
@@ -0,0 +1,14 @@
+﻿i18next
+    .use(window.i18nextXHRBackend)
+    .init({
+        debug: true,
+        fallbackLng: "tr",
+        backend: {
+            loadPath: "Assets/app-assets/data/locales/{{lng}}.json",
+        },
+        returnObjects: true
+    },
+        function (err, t) {
+
+            jqueryI18next.init(i18next, $);
+        });
